e51eb8252f2c051aa41f46318a344b39fee5a8cb
vandermulya <vandermulyaputra@gmail.com>
2024-01-28 16:41:57 +0700

    merge, create and run the server

---
+++ client-oleagroup/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/profile" element={<Profile />} />
+
+                </Routes>
+        </BrowserRouter>
+    );
+}
